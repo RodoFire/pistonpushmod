0ea49429295ee306fbc033132006d76ccff51bac
RodoFire <louisbriche@gmail.com>
2024-12-26 12:58:57 +0100

    update to 1.21.4

--- src/main/java/net/louis/pistonpushmod/mixin/PistonHandlerMixin.java
+++ src/main/java/net/louis/pistonpushmod/mixin/PistonHandlerMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.block.Blocks;
 import net.minecraft.block.PistonBlock;
 import net.minecraft.block.piston.PistonBehavior;
 import net.minecraft.block.piston.PistonHandler;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
@@ -58,71 +59,74 @@ public abstract class PistonHandlerMixin {
      */
     @Overwrite
     private boolean tryMove(BlockPos pos, Direction dir) {
-        int maxpush = world.getGameRules().getInt(PistonPushMod.PISTON_PUSHING_LIMIT);
-        int k;
-        BlockState blockState = this.world.getBlockState(pos);
-        if (blockState.isAir()) {
-            return true;
-        }
-        if (!PistonBlock.isMovable(blockState, this.world, pos, this.motionDirection, false, dir)) {
-            return true;
-        }
-        if (pos.equals(this.posFrom)) {
-            return true;
-        }
-        if (this.movedBlocks.contains(pos)) {
-            return true;
-        }
-        int i = 1;
-        if (i + this.movedBlocks.size() > maxpush) {
-            return false;
-        }
-        while (isBlockSticky(blockState)) {
-            BlockPos blockPos = pos.offset(this.motionDirection.getOpposite(), i);
-            BlockState blockState2 = blockState;
-            blockState = this.world.getBlockState(blockPos);
-            if (blockState.isAir() || !isAdjacentBlockStuck(blockState2, blockState) || !PistonBlock.isMovable(blockState, this.world, blockPos, this.motionDirection, false, this.motionDirection.getOpposite()) || blockPos.equals(this.posFrom))
-                break;
-            if (++i + this.movedBlocks.size() <= maxpush) continue;
-            return false;
-        }
-        int j = 0;
-        for (k = i - 1; k >= 0; --k) {
-            this.movedBlocks.add(pos.offset(this.motionDirection.getOpposite(), k));
-            ++j;
-        }
-        k = 1;
-        while (true) {
-            BlockPos blockPos2;
-            int l;
-            if ((l = this.movedBlocks.indexOf(blockPos2 = pos.offset(this.motionDirection, k))) > -1) {
-                this.setMovedBlocks(j, l);
-                for (int m = 0; m <= l + j; ++m) {
-                    BlockPos blockPos3 = this.movedBlocks.get(m);
-                    if (!isBlockSticky(this.world.getBlockState(blockPos3)) || this.tryMoveAdjacentBlock(blockPos3))
-                        continue;
-                    return false;
-                }
-                return true;
-            }
-            blockState = this.world.getBlockState(blockPos2);
+        if(world instanceof ServerWorld serv){
+            int maxpush = serv.getGameRules().getInt(PistonPushMod.PISTON_PUSHING_LIMIT);
+            int k;
+            BlockState blockState = this.world.getBlockState(pos);
             if (blockState.isAir()) {
                 return true;
             }
-            if (!PistonBlock.isMovable(blockState, this.world, blockPos2, this.motionDirection, true, this.motionDirection) || blockPos2.equals(this.posFrom)) {
-                return false;
-            }
-            if (blockState.getPistonBehavior() == PistonBehavior.DESTROY) {
-                this.brokenBlocks.add(blockPos2);
+            if (!PistonBlock.isMovable(blockState, this.world, pos, this.motionDirection, false, dir)) {
                 return true;
             }
-            if (this.movedBlocks.size() >= maxpush) {
+            if (pos.equals(this.posFrom)) {
+                return true;
+            }
+            if (this.movedBlocks.contains(pos)) {
+                return true;
+            }
+            int i = 1;
+            if (i + this.movedBlocks.size() > maxpush) {
                 return false;
             }
-            this.movedBlocks.add(blockPos2);
-            ++j;
-            ++k;
+            while (isBlockSticky(blockState)) {
+                BlockPos blockPos = pos.offset(this.motionDirection.getOpposite(), i);
+                BlockState blockState2 = blockState;
+                blockState = this.world.getBlockState(blockPos);
+                if (blockState.isAir() || !isAdjacentBlockStuck(blockState2, blockState) || !PistonBlock.isMovable(blockState, this.world, blockPos, this.motionDirection, false, this.motionDirection.getOpposite()) || blockPos.equals(this.posFrom))
+                    break;
+                if (++i + this.movedBlocks.size() <= maxpush) continue;
+                return false;
+            }
+            int j = 0;
+            for (k = i - 1; k >= 0; --k) {
+                this.movedBlocks.add(pos.offset(this.motionDirection.getOpposite(), k));
+                ++j;
+            }
+            k = 1;
+            while (true) {
+                BlockPos blockPos2;
+                int l;
+                if ((l = this.movedBlocks.indexOf(blockPos2 = pos.offset(this.motionDirection, k))) > -1) {
+                    this.setMovedBlocks(j, l);
+                    for (int m = 0; m <= l + j; ++m) {
+                        BlockPos blockPos3 = this.movedBlocks.get(m);
+                        if (!isBlockSticky(this.world.getBlockState(blockPos3)) || this.tryMoveAdjacentBlock(blockPos3))
+                            continue;
+                        return false;
+                    }
+                    return true;
+                }
+                blockState = this.world.getBlockState(blockPos2);
+                if (blockState.isAir()) {
+                    return true;
+                }
+                if (!PistonBlock.isMovable(blockState, this.world, blockPos2, this.motionDirection, true, this.motionDirection) || blockPos2.equals(this.posFrom)) {
+                    return false;
+                }
+                if (blockState.getPistonBehavior() == PistonBehavior.DESTROY) {
+                    this.brokenBlocks.add(blockPos2);
+                    return true;
+                }
+                if (this.movedBlocks.size() >= maxpush) {
+                    return false;
+                }
+                this.movedBlocks.add(blockPos2);
+                ++j;
+                ++k;
+            }
         }
+        return false;
     }
 
     /**
